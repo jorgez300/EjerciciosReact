d6b33a64fb1341d1dad063f0ce0bb7f363c242b7
jorgez300 <ing.jorge.gonzalez92@gmail.com>
2023-08-22 13:59:20 -0400

    inicio

---
+++ EjerciciosReact/ReactVite/src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const metodo = (): void => {
+
+    alert('hello');
+    const Div = document.getElementById('midiv') as HTMLDivElement;
+    Div.style.border = 'solid 1px';
+    setCount((count) => count + 1);
+  }
+
+  return (
+    <>
+      <div id='midiv'>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={metodo}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
